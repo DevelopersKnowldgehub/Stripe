a803a1bc9fbe2269edf57e9f39abf226a2838c87
DevelopersKnowldgehub <50870710+DevelopersKnowldgehub@users.noreply.github.com>
2019-05-21 19:47:03 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$plaid_client='YOUR PLAID CLIENT ID';/*YOUR api test*/
+$plaid_secret='YOUR PLAID SECRET';/*YOUR api test*/
+$stripe_secret_key='YOUR STRIPE SECRET';/*YOUR Test*/
+$stripe_publication_key='YOUR STRIPE PUBLICATION KEY';/*YOUR api for test*/
+?>
